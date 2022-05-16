086dad4597e73e9006a4974e477d675b51d7ce00
Krystian Jachna <krysti4nape@gmail.com>
2022-05-16 13:37:12 +0200

    Pierwsze podejscie do ogrodow

---
+++ BaCa_OwocowyOgrod/BaCa_OwocowyOgrod/BaCa_OwocowyOgrod.cpp
@@ -0,0 +1,38 @@
+﻿// Krystian Jachna
+#include <iostream>
+using namespace std;
+
+class GARDEN_CLASS {
+private:
+    unsigned int treesNumber;
+    TREE_CLASS trees;
+
+};
+
+class TREE_CLASS {
+private: 
+    unsigned int treeID;
+    unsigned int treeHight;
+    unsigned int branchNumber;
+    BRANCH_CLASS branches;
+
+};
+
+class BRANCH_CLASS {
+private:
+    unsigned int branchLength;
+    unsigned int fruitsNumber;
+    FRUIT_CLASS fruits;
+
+};
+
+class FRUIT_CLASS {
+private:
+    unsigned int weight;
+
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
