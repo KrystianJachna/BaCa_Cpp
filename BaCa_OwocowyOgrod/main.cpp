adc5de465d34bf442311498751e3eec9eae85774
Krystian Jachna <krysti4nape@gmail.com>
2022-05-19 19:44:05 +0200

    Test

--- BaCa_OwocowyOgrod/main.cpp
+++ BaCa_OwocowyOgrod/main.cpp
@@ -1,15 +1,66 @@
-// Krystian Jachna
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
 using namespace std;
 
-int main() {
+string imie, nazwisko;
 
-    dasdada
-    das
-    dad
-    asd
-    a
+int nr_tel;
+
+int main()
+{
+    fstream plik;
+    plik.open("wizytowka.txt", ios::in);//ios::in czyli wprowadz do programu
+
+    if (plik.good() == false)
+    {
+        cout << "Plik nie istnieje!";
+        exit(0);
+    }
+
+    string linia;
+    int nr_lini = 1;
+
+    while (getline(plik, linia))
+    {
+        switch (nr_lini)
+        {
+        case 1: imie = linia; break;
+        case 2: nazwisko = linia; break;
+        case 3: nr_tel = atoi(linia.c_str()); break;
+        }
+        
+        nr_lini++;
+
+
+    }
+
+    plik.close();
+
+
+    cout << imie << endl;
+    cout << nazwisko << endl;
+    cout << nr_tel << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     return 0;
+
 }
