600e67cfdb53dc5681884584163ab2eb3b41974d
Krystian Jachna <krysti4nape@gmail.com>
2022-05-20 02:39:27 +0200

    FRUIT_CLASS

--- BaCa_OwocowyOgrod/main.cpp
+++ BaCa_OwocowyOgrod/main.cpp
@@ -1,16 +1,43 @@
 // Krystian Jachna
 #include <iostream>
 
+class GARDEN_CLASS;
+class TREE_CLASS;
+class BRANCH_CLASS;
+class FRUIT_CLASS;
+
+class TREE_CLASS {
+
+};
 
 class BRANCH_CLASS {
+private:
+    unsigned int branchLength;
+    TREE_CLASS* branchTree;
+    FRUIT_CLASS* firstFruit;
+    FRUIT_CLASS* lastFruit;
+    unsigned int fruitsNumber;
+    unsigned int fruitsWeight;
+    unsigned int treeHeight;
 
+public:
+    unsigned int getFruitsTotal();
+    unsigned int getWeightsTotal();
+    unsigned int getHeight();
+    unsigned int getLength();
+    void growthBranch();
+    void fadeBranch();
+    void harvestBranch(unsigned int fruitWeight);
+    void cutBranch (unsigned int branchLength);
+    FRUIT_CLASS* getFruitPointer(unsigned int length);
+    TREE_CLASS* getTreePointer();
 };
 
 class FRUIT_CLASS {
 private:
     BRANCH_CLASS* fruitBranch;
     unsigned int fruitWeight;
-    unsigned int fruitBranchLength;
+    unsigned int fruitBranchLength;         // na jakiej dlugosci jest owoc
     FRUIT_CLASS* next;
     FRUIT_CLASS* prev;
 
@@ -21,7 +48,8 @@ public:
     void fadeFruit();
     void pluckFruit();
     BRANCH_CLASS* getBranchPointer();
-    FRUIT_CLASS() ;
+    FRUIT_CLASS(BRANCH_CLASS* fruitBranch, unsigned int fruitWeight, unsigned int fruitBranchLength);
+    ~FRUIT_CLASS();
 };
 
 unsigned int FRUIT_CLASS::getLength() {
@@ -47,6 +75,26 @@ void FRUIT_CLASS::pluckFruit() {
 BRANCH_CLASS* FRUIT_CLASS::getBranchPointer() {
     return fruitBranch;
 }
+
+FRUIT_CLASS::FRUIT_CLASS(BRANCH_CLASS* fruitBranch, unsigned int fruitWeight, unsigned int fruitBranchLength) {
+    this->fruitBranch = fruitBranch;
+    this->fruitWeight = fruitWeight;
+    this->fruitBranchLength = fruitBranchLength;
+
+    this->next = NULL;
+    this->prev = NULL;
+}
+
+FRUIT_CLASS::~FRUIT_CLASS() {
+    if (this->next != NULL && this->prev != NULL) {
+        this->prev->next = this->next;
+        this->next->prev = this->prev;
+    }
+}
+
+
+
+
 
 int main() {
 
